e1f56b3d718e0852bb28452ef54501a008f35e66
junjie wang <junjie7311@gmail.com>
2025-10-06 22:36:49 +0200

    Correccion de fallo en eliminar

--- src/routes/apuntadoRoutes.ts
+++ src/routes/apuntadoRoutes.ts
@@ -4,7 +4,7 @@ import * as apuntadoController from '../controller/apuntadoController';
 const router = Router();
 
 router.post('/', apuntadoController.addApuntadoHandler);
-router.delete('/', apuntadoController.removeApuntadoHandler);
+router.delete('/:id', apuntadoController.removeApuntadoHandler);
 router.get('/evento/:eventoId', apuntadoController.getApuntadosByEventoHandler);
 router.get('/user/:userId', apuntadoController.getEventosByUserHandler);
 router.get('/', apuntadoController.getAllApuntadosHandler);
